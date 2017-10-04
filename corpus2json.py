79c04ca08042981c094e4fbe0a1c0bf7d0353588
motaz.saad <motaz.saad@gmail.com>
2017-10-04 02:32:45 -0400

    finalize a stable version and adding json corpus

--- corpus2json.py
+++ corpus2json.py
@@ -17,15 +17,18 @@ def corpus2json(dir, json_out, level):
                 print('processing {}'.format(f))
                 filename = os.path.basename(f)
                 doc = open(f).read()
-                json.dump({'text': doc, 'label': label, 'filename': filename}, json_writer)
+                json.dump({'text': doc, 'label': label, 'filename': filename}, json_out)
                 json_out.write('\n')
     elif level == 'sentence':
-        labels =   glob.glob(dir + '/*')
-        print('labels: {}'.format(labels))
-        for label in labels:
-          lines = open(label).readlines()
-          for i, line in enamurate(lines):
-            json.dump({'text': line, 'label': label, 'id': i}, json_writer)
+        files =   glob.glob(dir + '/*')
+        print('files: {}'.format(files))
+        for f in files:
+          print('processing {}'.format(f))
+          lines = open(f, encoding='utf-8').read().splitlines()
+          print('{} has {} lines'.format(f, len(lines)))
+          fname = os.path.basename(f)
+          for i, line in enumerate(lines):
+            json.dump({'text': line, 'label': fname, 'id': i}, json_out)
             json_out.write('\n')
             
 
@@ -44,3 +47,4 @@ if __name__ == '__main__':
     output = args.outfile
     level = args.level
     corpus2json(input, output, level)
+    print('done!')
